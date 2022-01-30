c6cf9721a5a19bee7f4197444c691138d3f18162
Raja Mohamed <techraja2020@gmail.com>
2022-01-30 22:10:12 +0530

    karthi merge

--- MyFirstCoreAPI/Startup.cs
+++ MyFirstCoreAPI/Startup.cs
@@ -38,11 +38,12 @@ namespace MyFirstCoreAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            
+            //if (env.IsDevelopment())
+            //{
+            //Added By Karthi from branch1
+            //app.UseDeveloperExceptionPage();
+            //}
+
             app.UseRouting();
             app.UseStaticFiles();
              app.UseAuthorization();
